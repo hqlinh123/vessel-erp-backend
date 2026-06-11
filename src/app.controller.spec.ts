c65cef6ea04a0f1c1b8848a64c069e99758b30f6
santiago <santiago@MacBookPro.lan>
2026-06-11 17:48:49 +0700

    fix build

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import assert from 'node:assert';
+import { beforeEach, describe, it } from 'node:test';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -16,7 +18,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      assert.strictEqual(appController.getHello(), 'Hello World!');
     });
   });
 });
