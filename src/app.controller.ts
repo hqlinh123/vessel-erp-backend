09db24c300f9f4f0062ca4bc3e863788ade78f87
santiago <linhdevios@gmail.com>
2026-06-17 21:54:29 +0700

    test domain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/health')
-  async health(){
+  async health() {
     return {
       message: 'ok',
     };
--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
 
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api/docs', app, document);
-  const port = process.env.PORT ?? 3000
+  const port = process.env.PORT ?? 8080;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`API prefix: /api`);
